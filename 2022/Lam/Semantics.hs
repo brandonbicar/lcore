6d5255be5e7501cc2a17ee6740347f62eff21c55
Brandon Bicar <github@bicar.io>
2023-02-06 19:38:25 +0000

    freshen bound variable name to avoid variable capture in subst function

--- 2022/Lam/Semantics.hs
+++ 2022/Lam/Semantics.hs
@@ -1,6 +1,7 @@
 module Lam.Semantics where
 
 import Lam.Syntax
+import Data.List (nub, delete)
 
 -- reduce to normal form with number of reductions
 multiStep :: Expr -> (Expr, Int)
@@ -26,4 +27,36 @@ subst (Var y) t' x | y == x    = t'
                    | otherwise = Var y
 subst (App t1 t2) t' x         = App (subst t1 t' x) (subst t2 t' x)
 subst (Abs y t) t' x | x == y    = Abs y t
-                     | otherwise = Abs y (subst t t' x)
+                     | otherwise = if y `elem` freeVars t'
+                                     then 
+                                       let freshVar = y ++ "'"
+                                           freshened = subst t (Var freshVar) y
+                                       in Abs freshVar (subst freshened t' x)
+                                     else Abs y (subst t t' x)
+
+freeVars :: Expr -> [Identifier]
+freeVars (Var x)     = [x]
+freeVars (App t1 t2) = freeVars t1 ++ freeVars t2
+freeVars (Abs x t)   = delete x (nub (freeVars t))
+
+
+-- (\x -> \y -> x) (y a)
+-- \y' -> y a
+--
+-- multiStep App (Abs "x" (Abs "y" (Var "x"))) (App (Var "y") (Var "a"))
+--
+-- step App (Abs "x" (Abs "y" (Var "x"))) (App (Var "y") (Var "a"))
+--
+-- x = "x", t = (Abs "y" (Var "x")), t' = (App (Var "y") (Var "a"))
+--
+-- Just (subst (Abs "y" (Var "x")) (App (Var "y") (Var "a")) "x")
+--
+-- y = "y", t = (Var "x"), t' = (App (Var "y") (Var "a")), x = "x"
+--
+-- Abs "y" (subst (Var "x") (App (Var "y") (Var "a")) ("x"))
+--
+-- y = "x", t' = (App (Var "y") (Var "a")), x = "x"
+--
+-- (App (Var "y") (Var "a"))
+--
+-- Just (Abs "y'" (App (Var "y") (Var "a")))
